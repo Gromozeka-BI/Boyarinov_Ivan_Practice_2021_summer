6c460955bceb2de752c6dc0ec4f180fdd3736ae4
Gromozeka-BI <gromozeka-bi@yandex.ru>
2021-07-08 14:31:41 +0300

    final

--- Wolves and Sheep/main.cpp
+++ Wolves and Sheep/main.cpp
@@ -2,6 +2,9 @@
 #include <math.h>
 #include <string>
 #include <stdio.h>
+#include <sstream>
+#include <windows.h>
+#include <cstdlib>
 //#include <system>
 
 using namespace std;
@@ -48,10 +51,60 @@ void outputChessBoard(int board[8][8]){
                     cout << "   ";
                 }
                 cout << "|";
+
+
         }
 
-    cout << " " << j + 1 <<endl;
+    cout << " " << j + 1;
+
+    if (j == 7) {
+        cout<< "\t\t\t" << "Волки и Овца";
+    }
+    if (j == 6) {
+        cout<< "\t" << "Правила игры:";
+    }
+    if (j == 5) {
+        cout<< "\t" << "2)Волки ходят на одну 1 клетку по диагонали, но только вперёд";
+    }
+    if (j == 4) {
+        cout<< "\t" << "4)Волки побеждаю, когда овца не может никуда ходить";
+    }
+    if (j == 3) {
+        cout<< "\t" << "куда она должна сходить";
+    }
+
+    if (j == 2) {
+        cout<< "\t" << "откуда волк должен сделать ход и через '-' куда он должен сходить";
+    }
+
+    if (j == 1) {
+        cout<< "\t\t\t " << "Пример ввода:";
+    }
+    if (j == 0) {
+        cout<< "\t" << "Ob8-c7                               Bh1-g2  ";
+    }
+
+    cout << endl;
     cout << "|___|___|___|___|___|___|___|___|__";
+
+
+    if (j == 6) {
+        cout<< "\t" << "1)Овца ходит только на 1 клетку по диагонали";
+    }
+    if (j == 5) {
+        cout<< "\t" << "3)Овца побеждает, когда доходит до 1 линии";
+    }
+    if (j == 4) {
+        cout<< "\t" << "5)Чтобы сделать ход за овцу, необходимо ввести клетку,";
+    }
+
+    if (j == 3) {
+        cout<< "\t" << "6)Чтобы сделать ход за волка, необходимо ввести клетку,";
+    }
+    if (j == 1) {
+        cout<< "\t" << "Ходит овца:                          Ходит волк:  ";
+    }
+
     cout << endl;
     }
     cout << "  a   b   c   d   e   f   g   h";
@@ -141,20 +194,55 @@ void step(int animal){
         mStep[i] = -1;
     }
 
+    //cout << "O";
+
+
+
     char input[6];//количество введёных символов
 
-    char symbol = getchar();
-    int number = 0;
-    while(symbol != '\n'){
-        for(int i = 0; i < 6; i++){
-            if (number == i){
-                input[i] = symbol;
-            }
+    if (animal == 1){
+        cout << 'O';
+        cout << (char)(sheep[0] + 'a');
+        cout << (char)(sheep[1] + '1');
+        cout << '-';
+        char symbol = getchar();
 
+        int number = 4;
+        while(symbol != '\n'){
+            for(int i = 4; i < 6; i++){
+                if (number == i){
+                    input[i] = symbol;
+                }
+
+            }
+            number++;
+            symbol = getchar();
         }
-        number++;
-        symbol = getchar();
+        input[0] = 'O';
+        input[1] = (char)(sheep[0] + 'a');
+        input[2] = (char)(sheep[1] + '1');
+        input[3] = '-';
     }
+
+    if (animal == 2){
+        cout << 'B';
+        char symbol = getchar();
+
+        int number = 1;
+        while(symbol != '\n'){
+            for(int i = 1; i < 6; i++){
+                if (number == i){
+                    input[i] = symbol;
+                }
+
+            }
+            number++;
+            symbol = getchar();
+        }
+        input[0] = 'B';
+    }
+
+    //cout << input[0] << input[1] << input[2] << input[3] << input[4] << input[5] <<endl;
 
     if((input[0] == 'O') && (animal == 1) && (input[3] == '-')){//проверка для овцы
         mStep[0] = animal;
@@ -171,6 +259,8 @@ void step(int animal){
         mStep[4] = numb(input[5]);
     }
 
+    //cout << mStep[0] << mStep[1] << mStep[2] << mStep[3] << mStep[4] <<endl;
+
     int erorr = 0;
     for (int i = 0; i < 5; i++){
         if (mStep[i] == -1){
@@ -182,6 +272,7 @@ void step(int animal){
         mStep[i] = -1;
         }
     }
+    //cout << erorr << endl;
 
 }
 
@@ -193,45 +284,76 @@ void outputStepTest(){
 }
 
 int whoWin(){
-    if (sheep[0] == 0){//если находится слева и зажата, то проигрышь
-            if (board[sheep[0] + 1][sheep[1] - 1] == 2){
-                return 2;
-            }
-        }
-        if (sheep[0] == 7){//если находится справа и зажата, то проигрышь
-            if (board[sheep[0] - 1][sheep[1] - 1] == 2){
-                return 2;
-            }
-        }
 
-        if ((board[sheep[0] + 1][sheep[1] - 1] == 2) && (board[sheep[0] - 1][sheep[1] - 1] == 2)){// если зажата по бокам
-            return 2;
-        }
-
-
-        for (int i = 0; i < 8; i++){// если овца дошла до конца, то она победила
+    for (int i = 0; i < 8; i++){// если овца дошла до конца, то она победила
             if (board[i][0] == 1){
+                //cout << "win1";
                 return 1;
             }
         }
+
+    if (sheep[1] < 7){
+        if (sheep[0] == 0){//если находится слева (не на 8 строке) и зажата, то проигрышь
+            if ((board[sheep[0] + 1][sheep[1] - 1] == 2) && (board[sheep[0] + 1][sheep[1] + 1] == 2)){
+                //cout << "win2";
+                return 2;
+            }
+        }
+    }else{
+        if (sheep[0] == 0){//если находится слева (на 8 строке) и зажата, то проигрышь
+            if (board[sheep[0] + 1][sheep[1] - 1] == 2){
+                //cout << "win3";
+                return 2;
+            }
+        }
+    }
+
+    if (sheep[1] < 7){
+        if (sheep[0] == 7){//если находится справа (не на 8 строке) и зажата, то проигрышь
+            if ((board[sheep[0] - 1][sheep[1] - 1] == 2) && (board[sheep[0] - 1][sheep[1] + 1] == 2)){
+                //cout << "win4";
+                return 2;
+            }
+        }
+    }else{
+        if (sheep[0] == 7){//если находится справа (на 8 строке) и зажата, то проигрышь
+            if (board[sheep[0] - 1][sheep[1] - 1] == 2){
+                //cout << "win5";
+                return 2;
+            }
+        }
+    }
+
+    if (sheep[1] < 7){
+        if ((board[sheep[0] + 1][sheep[1] - 1] == 2) && (board[sheep[0] - 1][sheep[1] - 1] == 2) && (board[sheep[0] + 1][sheep[1] + 1] == 2) && (board[sheep[0] - 1][sheep[1] + 1] == 2)){// если зажата по бокам на 8 линии
+            //cout << "win6";
+            return 2;
+        }
+    }else{
+        if ((board[sheep[0] + 1][sheep[1] - 1] == 2) && (board[sheep[0] - 1][sheep[1] - 1] == 2)){// если зажата по бокам на 8 линии
+            //cout << "win7";
+            return 2;
+        }
+    }
     return 0;
 }
 
-int PVP(){
+int PVP(int shOrWolf){
 
 
     int winner = -1;
     bool stop = true;
-    int shOrWolf = 0;//счетчик ходов
+    //shOrWolf = 0;//счетчик ходов
 
 
     while (stop){
         system ("cls");
         outputChessBoard(board);
         if (shOrWolf % 2 == 0){//ход Овцы
-            cout << "Sheep move:" << endl;
+            cout << "Ходит овца:" << endl;
             bool correct = false;
             step(1);
+            int inputError = 0;
             while (!correct){
                 if (mStep[0] != -1){
                     if((board[mStep[1]][mStep[2]] == 1) && (board[mStep[3]][mStep[4]] == 0) && ((mStep[2] == mStep[4] + 1) || (mStep[2] == mStep[4] - 1)) && ((mStep[1] == mStep[3] - 1) || (mStep[1] == mStep[3] + 1))){
@@ -241,16 +363,31 @@ int PVP(){
                         sheep[1] = mStep[4];
                         correct = !correct;
                     }else{
+                        inputError++;
+                        system ("cls");
+                        outputChessBoard(board);
+                        cout << "Попыток неправельного ввода: " << inputError << endl;
+                        cout << "Повторите ввод" << endl;
+                        cout << "Ходит овца:" << endl;
                         step(1);
+                        //cout << "erorr1" << endl;
                     }
                 }else{
-                    step(1);
+                    //cout << "erorr2" << endl;
+                        inputError++;
+                        system ("cls");
+                        outputChessBoard(board);
+                        cout << "Попыток неправельного ввода: " << inputError << endl;
+                        cout << "Повторите ввод" << endl;
+                        cout << "Ходит овца:" << endl;
+                        step(1);
                 }
             }
         }else{//ход волка
-            cout << "Wolf move:" << endl;
+            cout << "Ходит волк:" << endl;
             bool correct = false;
             step(2);
+            int inputError = 0;
             while (!correct){
                 if (mStep[0] != -1){
                     if((board[mStep[1]][mStep[2]] == 2) && (board[mStep[3]][mStep[4]] == 0) && (mStep[2] == mStep[4] - 1) && ((mStep[1] == mStep[3] - 1) || (mStep[1] == mStep[3] + 1))){
@@ -258,9 +395,21 @@ int PVP(){
                         board[mStep[3]][mStep[4]] = 2;
                         correct = !correct;
                     }else{
+                        inputError++;
+                        system ("cls");
+                        outputChessBoard(board);
+                        cout << "Попыток неправельного ввода: " << inputError << endl;
+                        cout << "Повторите ввод" << endl;
+                        cout << "Ходит волк:" << endl;
                         step(2);
                     }
                 }else{
+                    inputError++;
+                    system ("cls");
+                    outputChessBoard(board);
+                    cout << "Попыток неправельного ввода: " << inputError << endl;
+                    cout << "Повторите ввод" << endl;
+                    cout << "Ходит волк:" << endl;
                     step(2);
                 }
             }
@@ -279,65 +428,136 @@ int PVP(){
     return winner;
 }
 
-int main(){
-    clearBoard(board);
-
-    //Выбор клетки, с которой начинает ходить овца
-    int OStart = -1;
+bool repeat(){
     int inputError = 0;
-    //OStart = 1; // b8
-    //OStart = 2; // d8
-    //OStart = 3; // f8
-    //OStart = 4; // h8
+    bool erorr = true;
+        while(erorr){
+            //system ("cls");
+            if (inputError == 0){
+                cout << "Чтобы начать игру заново введите: '1'" << endl;
+                cout << "Чтобы закрыть игру введите: '0'" << endl;
+            }else{
+                system ("cls");
+                cout << "Попыток неправельного ввода: " << inputError << endl;
+                cout << "Повторите попытку" << endl;
+                cout << "Чтобы начать игру заново введите: '1'" << endl;
+                cout << "Чтобы закрыть игру введите: '0'" << endl;
+            }
 
-    while(OStart == -1){
-        if (inputError == 0){
-            cout << "Enter the value at which the sheep begins to walk" << endl;
-            cout << "Possible starting cells: 'b8', 'd8', 'f8', 'h8'" << endl;
-        }else{
-            cout << "Do you missing the buttons?" << endl;
-            cout << "Possible starting cells: 'b8', 'd8', 'f8', 'h8'" << endl;
+            char input[1];
+            char symbol = getchar();
+            int number = 0;
+            while(symbol != '\n'){
+                for(int i = 0; i < 1; i++){
+                    if (number == i){
+                        input[i] = symbol;
+                    }
+
+                }
+                number++;
+                symbol = getchar();
+            }
+            if (input[0] == '0'){
+                system ("cls");
+                return false;
+            }
+            if (input[0] == '1'){
+                system ("cls");
+                return true;
+            }
+
+            inputError++;
         }
-        OStart = locate();
-        inputError++;
+}
+
+int main(){
+    bool rep = true;
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL,"russian");
+    //rep = repeat();
+
+    while (rep){
+        clearBoard(board);
+
+        //Выбор клетки, с которой начинает ходить овца
+        int OStart = -1;
+        int inputError = 0;
+        //OStart = 1; // b8
+        //OStart = 2; // d8
+        //OStart = 3; // f8
+        //OStart = 4; // h8
+
+
+        ///*
+        while(OStart == -1){
+            if (inputError == 0){
+                system ("cls");
+                cout<< "Волки и Овца"<< endl;
+                cout << endl;
+                cout << "Введите клетку, с которой овца начнет ходить" << endl;
+                cout << "Возможные варианты: 'b8', 'd8', 'f8', 'h8'" << endl;
+            }else{
+                system ("cls");
+                cout << "Попыток неправельного ввода: " << inputError << endl;
+                cout << "Повторите попытку" << endl;
+                cout << "Введите клетку, с которой овца начнет ходить" << endl;
+                cout << "Возможные варианты: 'b8', 'd8', 'f8', 'h8'" << endl;
+            }
+            OStart = locate();
+            inputError++;
+        }
+
+        inputError = 0;
+        inputStart(OStart);
+        //*/
+
+
+           /*
+        board[0][6] = 1;
+        board[1][5] = 2;
+        board[2][6] = 2;
+        sheep[0] = 0;
+        sheep[1] = 6;
+        //outputBoardTest(board);
+        //outputChessBoard(board);
+        */
+
+
+        int endd = PVP(0);
+
+        if (endd == 1){
+            system ("cls");
+            outputChessBoard(board);
+            cout << "Победила овца" << endl;
+            cout << endl;
+            rep = repeat();
+        }
+        if (endd == 2){
+            system ("cls");
+            outputChessBoard(board);
+            cout << "Победили волки" << endl;
+            cout << endl;
+            rep = repeat();
+        }
+        if (endd == -1){
+            system ("cls");
+            outputChessBoard(board);
+            cout << "Erorr output" << endl;
+            cout << endl;
+            rep = repeat();
+        }
+
+        //step(1);// ход овцы
+        //outputStepTest();
+
+        //step(2);// ход волка
+        //outputStepTest();
     }
-    inputError = 0;
-    inputStart(OStart);
 
-    /*
-    board[0][6] = 1;
-    board[1][5] = 2;
-    sheep[0] = 0;
-    sheep[1] = 6;
-    outputBoardTest(board);
-    outputChessBoard(board);
-    */
-
-
-    int endd = PVP();
-
-    if (endd == 1){
-        system ("cls");
-        outputChessBoard(board);
-        cout << "Sheep winner" << endl;
-    }
-    if (endd == 2){
-        system ("cls");
-        outputChessBoard(board);
-        cout << "Wolfs winner" << endl;
-    }
-    if (endd == -1){
-        system ("cls");
-        outputChessBoard(board);
-        cout << "Erorr" << endl;
-    }
-
-    step(1);// ход овцы
-    outputStepTest();
-
-    //step(2);// ход волка
-    //outputStepTest();
 
 
     return 0;
+    exit(0);
 }
